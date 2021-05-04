be34605da974cd6c4339cb726e23afe821f22659
Yugo <hugues.vandenbroucke@gmail.com>
2021-05-04 14:42:27 +0200

    Fine-tuning of the PDF publishing framework

--- scripts/publish_pdf.sh
+++ scripts/publish_pdf.sh
@@ -10,6 +10,6 @@ CONFIG="../transform/fop.print.cfg"
 SOURCE_XML="../core.xml"
 SOURCE_XSL="../transform/print.xsl"
 
-OUTPUT="../published/pdf/CORE.pdf"
+OUTPUT="../published/pdf/CORE_-_Cooperative_Open_Roleplaying_Engine.pdf"
 
 fop -xml $SOURCE_XML -xsl $SOURCE_XSL -pdf $OUTPUT -c $CONFIG
